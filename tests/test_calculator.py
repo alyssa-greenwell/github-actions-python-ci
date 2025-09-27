cb6db3a9387e78703638d4db8bf3a0cbdfcd3733
Alyssa Greenwell <808alyssag@gmail.com>
2025-09-27 11:27:10 -1000

    Fixed indentation in test_calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,7 +2,7 @@ import unittest
 from src.calculator import add, subtract, divide
 
 class TestCalculator(unittest.TestCase):
-   def test_add(self):
+    def test_add(self):
        self.assertEqual(add(2, 3), 5)
        self.assertEqual(add(-1, 1), 0)
        self.assertEqual(add(-1, -1), -2)
